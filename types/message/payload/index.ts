495c865c4fba999945a9022e177fe8a68cbe4899
sweshelo@gmail.com <sweshelo@gmail.com>
2025-03-21 14:08:00 +0900

    型のexportを纏める

--- types/message/payload/index.ts
+++ types/message/payload/index.ts
@@ -1,4 +1,8 @@
 import type { PlayerEntryPayload } from "./room";
 import type { RoomOpenRequestPayload, RoomOpenResponsePayload } from "./server";
 
+export type * from "./base";
+export type * from "./room";
+export type * from "./server";
+
 export type Payload = RoomOpenRequestPayload | RoomOpenResponsePayload | PlayerEntryPayload
