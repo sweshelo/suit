a39df7385925b680f1799d4ab2b754af22dd745d
sweshelo@gmail.com <sweshelo@gmail.com>
2025-03-24 23:17:08 +0900

    client向けPayloadを追加

--- types/message/payload/index.ts
+++ types/message/payload/index.ts
@@ -1,8 +1,10 @@
+import type { SyncPayload } from "./client";
 import type { PlayerEntryPayload } from "./room";
 import type { RoomOpenRequestPayload, RoomOpenResponsePayload } from "./server";
 
 export type * from "./base";
 export type * from "./room";
 export type * from "./server";
+export type * from "./client";
 
-export type Payload = RoomOpenRequestPayload | RoomOpenResponsePayload | PlayerEntryPayload
+export type Payload = RoomOpenRequestPayload | RoomOpenResponsePayload | PlayerEntryPayload | SyncPayload
